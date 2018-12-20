759036bf77f937f2ab7ce681b5d396b3d87274d4
demns <demn_@tut.by>
2018-12-21 02:29:08 +0300

    Added deers

--- index.js
+++ index.js
@@ -24,11 +24,13 @@ var platforms;
 var visibleGround;
 var invisibleGround;
 var currentHouse;
-var fuel = 1000000;
+var fuel = 10000;
 var distance = 0;
 var maxDistance = 0;
 var groundPerTimeMovement = 4;
 var fuelText, distanceText, maxDistance;
+var deer;
+var currentDeer;
 
 function preload() {
     this.load.image('sky', 'assets/starry-night.jpg');
@@ -40,14 +42,12 @@ function preload() {
 
     this.load.image('star', 'assets/star.png');
     this.load.image('santa', 'assets/santa.png');
-    // this.load.image('bomb', 'assets/bomb.png');
-    // this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('deer', 'assets/deer2.png', { frameWidth: 32, frameHeight: 32 });
 }
 
 function create() {
     background = this.add.tileSprite(0, 0, 1600, 1200, 'sky');
     cursors = this.input.keyboard.createCursorKeys();
-
 
     platforms = this.physics.add.staticGroup();
     invisibleGround = platforms.create(400, 568, 'ground').setScale(2).refreshBody();
@@ -68,6 +68,19 @@ function create() {
     fuelText = this.add.text(16, 16, 'Deers: ' + fuel, { fontSize: '32px', fill: '#35a930' });
     distanceText = this.add.text(400, 16, 'Distance: 0', { fontSize: '32px', fill: '#35a930' });
     maxDistanceText = this.add.text(400, 550, 'Maximum: 0', { fontSize: '32px', fill: '#35a930' });
+
+    deer = this.physics.add.sprite(-300, 450, 'deer').setScale(1.5);
+    deer.body.setGravityY(-250);
+    deer.setBounce(0.9);
+    this.physics.add.collider(deer, platforms);
+    this.physics.add.overlap(player, deer, collectDeer, null, this);
+
+    this.anims.create({
+        key: 'run',
+        frames: this.anims.generateFrameNumbers('deer', { start: 0, end: 4 }),
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
 function update() {
@@ -75,11 +88,13 @@ function update() {
     visibleGround.tilePositionX += groundPerTimeMovement;
     distance++;
     distanceText.setText('Distance: ' + distance);
+    fuelText.setText('Deers: ' + fuel);
     if (currentHouse) {
         if (Math.abs(currentHouse.y - player.y) < 50 && Math.abs(currentHouse.x - player.x) < 50) {
             currentHouse.x = -100;
             currentHouse = null;
             fuel += 10000;
+            player.setVelocityY(-530);
         } else {
             currentHouse.x -= groundPerTimeMovement;
         }
@@ -88,20 +103,38 @@ function update() {
             currentHouse = null;
         }
     }
+
+    if (currentDeer) {
+        currentDeer.x -= (groundPerTimeMovement - 1);
+
+        if (currentDeer && currentDeer.x < -70) {
+            currentDeer.x = -100;
+            currentDeer = null;
+        }
+    }
+
     if (distance % 150 === 0 && !currentHouse) {
         currentHouse = getRandomHouse();
         currentHouse.x = 900;
     }
 
+    if (distance % 250 === 0 && !currentDeer) {
+        currentDeer = deer;
+        deer.enableBody(true, true);
+        currentDeer.x = 900;
+        currentDeer.y = 400;
+    }
+
     if (cursors.up.isDown && fuel > 0) {
         player.setVelocityY(-100);
         fuel -= 100;
-        fuelText.setText('Deers: ' + fuel);
     }
 
-    if (player.body.touching.down) {
-        // gameOver();
+    if (player.body.touching.down && player.y > 507) {
+        gameOver();
     }
+
+    deer.anims.play('run', true);
 }
 
 function gameOver() {
@@ -121,4 +154,11 @@ function getRandomHouse() {
     if (a > 0.50) return house2;
     if (a > 0.75) return house3;
     return house4;
+}
+
+function collectDeer(player, deer) {
+    currentDeer.x = -100;
+    currentDeer = null;
+    fuel += 30000;
+    fuelText.setText('Deers: ' + fuel);
 }
